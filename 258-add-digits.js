54d698025d5906e3e76009bee7e9b6e12a8f7522
podiumdesu <wwhmomo@gmail.com>
2018-04-07 01:25:01 +0800

    add: 258-add-digits.js

---
+++ 258-add-digits.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+
+
+const getAddNum = (num) => {
+  let res = 0
+  while (num > 0) {
+    res += num % 10
+    num = parseInt(num / 10)
+  }
+  return res
+}
+var addDigits = function(num) {
+  let res = num
+  do {
+    res = getAddNum(res)
+  } while (res >= 10)
+  return res
+};
+
+// amazing solution
+// res % 9 
+console.log(addDigits(45))
