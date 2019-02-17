95c19da269c355cb30520fe34648928725b36663
podiumdesu <wwhmomo@gmail.com>
2019-02-17 19:52:24 +0800

    Update: Medium 11 from O(n^2) to O(n)

--- Medium-11-container-with-most-water.js
+++ Medium-11-container-with-most-water.js
@@ -3,9 +3,29 @@
  * @return {number}
  */
 
+// O(n)解法：就是左右一起尝试呗
+var maxArea = function (height) {
+  let left = 0,
+    right = height.length - 1
+  let max = 0
+  while (left < right) {
+    max = Math.max(max, Math.min(height[left], height[right]) * (right - left))
+    if (height[left] > height[right]) {
+      right--
+    } else {
+      left++
+    }
+  }
+  return max
+}
+
+
+
 // 思路：从数组的最后往前找，找到数组从前往后的第一个比最后一个数字小的数，再计算长度即可
 // 再从数组的最前往后找，同样的思路。取最大值
 // [1, 2]的情况就需要从前往后找
+// 但是这是 O(n^2)的算法，应当使用O(n)进行解答
+/*
 var maxArea = function (height) {
   let max = 0
   for (let j = height.length - 1; j >= 0; j--) {
@@ -27,7 +47,7 @@ var maxArea = function (height) {
 
   return max
 };
-
+*/
 a = maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])
-a = maxArea([1, 2])
+// a = maxArea([1, 2])
 console.log(a)
