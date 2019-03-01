6816f049c8d2dc768870323e7680cb8a725d7839
podiumdesu <wwhmomo@gmail.com>
2019-03-01 20:26:55 +0800

    Add: DFS solution

--- Medium-841-Keys-and-rooms.js
+++ Medium-841-Keys-and-rooms.js
@@ -22,6 +22,27 @@ var canVisitAllRooms = function (rooms) {
   return 1
 };
 
+function traversal(rooms, res, AllRoom) {
+  for (let i = 0, N = rooms.length; i < N; i++) {
+    // console.log("i = " + i)
+    // console.log(rooms)
+    if (!res[rooms[i]]) {
+      res[rooms[i]] = 1
+      // console.log("现在访问了房间" + rooms[i])
+
+      traversal(AllRoom[rooms[i]], res, AllRoom)
+    }
+  }
+}
+var canVisitAllRooms = function (rooms) {
+  let N = rooms.length
+  let res = new Array(N).fill(0)
+  res[0] = 1
+  traversal(rooms[0], res, rooms)
+  // console.log(res)
+  if (res.join("").indexOf("0") >= 0) return -1
+  return 1
+}
 console.log(canVisitAllRooms([
   [1, 3],
   [3, 0, 1],
