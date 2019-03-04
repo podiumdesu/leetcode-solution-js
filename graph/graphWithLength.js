e91d4896edd59535dbb02016ffb60c3fce613676
podiumdesu <wwhmomo@gmail.com>
2019-03-04 17:17:49 +0800

    Add: graph alg of Dijkstra & bellman-ford

---
+++ graph/graphWithLength.js
@@ -0,0 +1,10 @@
+let graphs = require('./graphSave')
+let a = graphs.a
+let b = graphs.b
+// a.showDistance(0)
+// console.log(a.BellmanFord(0))
+// console.log(a.FindShortestDistance(0, 2))
+// console.log(a.Dijkstra(0))
+// console.log(a.FindShortestDistance(0, 4))
+// console.log(b.Dijkstra(0))
+// console.log(b.FindShortestDistance(0, 4))
