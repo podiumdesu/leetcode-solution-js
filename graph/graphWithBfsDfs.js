ff2ca0f2e470eecc689569bb4df4b7920b3cf322
podiumdesu <wwhmomo@gmail.com>
2019-03-15 17:20:10 +0800

    Add: dfs & check loop

--- graph/graphWithBfsDfs.js
+++ graph/graphWithBfsDfs.js
@@ -13,43 +13,71 @@ function Graph(v) {
   this.bfs = bfs
   this.dfs = dfs
   this.dfs_visit = dfs_visit
+  this.checkLoop = checkLoop
+  this.loop = false
+  this.checkLoop_visit = checkLoop_visit
 }
 
-function bfs() {
-  let a = [0]
+function bfs(val) {
+  let a = [val]
   const res = []
-  this.visited[0] = true
+  this.visited[a[0]] = true
   while (a.length > 0) {
     for (let i = 0; i < this.adj[a[0]].length; i++) {
       if (!this.visited[this.adj[a[0]][i]]) { // 没有访问过
         a.push(this.adj[a[0]][i])
         this.visited[this.adj[a[0]][i]] = true
-      } else {
-        console.log("Yes")
       }
     }
     res.push(a.shift())
   }
-  console.log(res)
+  console.log(`BFS: ${res}`)
 }
 
-function dfs_visit(visitNode) {
+function checkLoop_visit(visitNode) {
+  this.visited[visitNode] = "GREY"
+  for (let i = 0; i < this.adj[visitNode].length; i++) {
+    if (this.visited[this.adj[visitNode][i]] === "WHITE") {
+      this.checkLoop_visit(this.adj[visitNode][i])
+    } else if (this.visited[this.adj[visitNode][i]] === "GREY") {
+      this.loop = true
+    }
+  }
+  this.visited[visitNode] = "BLACK"
+}
+
+function checkLoop() { // 检查是否有环路
+  for (let i = 0; i < this.vertices; i++) {
+    this.visited[i] = "WHITE"
+  }
+  for (let i = 0; i < this.vertices; i++) {
+    if (this.visited[i] === "WHITE") {
+      this.checkLoop_visit(i)
+    }
+  }
+  console.log(this.loop)
+}
+
+function dfs_visit(visitNode, res) {
   this.visited[visitNode] = true
-  console.log(visitNode)
+  res.push(visitNode)
   for (let i = 0; i < this.adj[visitNode].length; i++) {
     if (!this.visited[this.adj[visitNode][i]]) {
-      this.dfs_visit(this.adj[visitNode][i])
+      this.dfs_visit(this.adj[visitNode][i], res)
     }
   }
-  console.log("访问！" + visitNode)
 }
 
-function dfs() {
-  for (let i = 0; i < this.vertices; i++) {
-    if (!this.visited[i]) {
-      this.dfs_visit(i)
-    }
-  }
+function dfs(val) {
+  let res = []
+  this.dfs_visit(val, res)
+  // for (let i = 0; i < this.vertices; i++) { // 可能会有多个联通分量
+  //   if (!this.visited[i]) {
+  //     console.log("GGG")
+  //     this.dfs_visit(i, res)
+  //   }
+  // }
+  console.log(`dfs: ${res}`)
 }
 
 function addEdge(v, w) {
@@ -96,7 +124,10 @@ a.addEdge(0, 3)
 a.addEdge(0, 4)
 a.addEdge(3, 1)
 // a.addEdge(4, 1)
+a.addEdge(0, 1)
+
 a.addEdge(2, 4)
 a.showGraph()
-// a.bfs()
-a.dfs()
+a.dfs(3)
+// a.bfs(2)
+a.checkLoop()
